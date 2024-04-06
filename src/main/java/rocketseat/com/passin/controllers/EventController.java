e1540bb534a9e8e650bad501661c7bbcf5a66019
pbarsou <pablo.barsou@gmail.com>
2024-04-06 15:53:56 -0300

    Endpoint Creation

--- src/main/java/rocketseat/com/passin/controllers/EventController.java
+++ src/main/java/rocketseat/com/passin/controllers/EventController.java
@@ -2,10 +2,11 @@ package rocketseat.com.passin.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import rocketseat.com.passin.dto.event.EventIdDTO;
+import rocketseat.com.passin.dto.event.EventRequestDTO;
+import rocketseat.com.passin.dto.event.EventResponseDTO;
 import rocketseat.com.passin.services.EventService;
 
 @RestController
@@ -14,8 +15,17 @@ import rocketseat.com.passin.services.EventService;
 public class EventController {
     private final EventService service;
     @GetMapping("/{eventId}") // pega a partir da URL
-    public ResponseEntity<String> getEvent(@PathVariable String eventId) { // informando que a variável vai vir da URL (Path Variable)
-        this.service.getEventDetail(eventId);
-        return ResponseEntity.ok("sucesso!");
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String eventId) { // informando que a variável vai vir da URL (Path Variable)
+        EventResponseDTO event = this.service.getEventDetail(eventId);
+        return ResponseEntity.ok(event);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder) {
+        EventIdDTO eventIdDTO = this.service.createEvent(body);
+
+        var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri(); // criando uma URI de devolução (para o usuário poder consultar informações do evento)
+
+        return ResponseEntity.created(uri).body(eventIdDTO);
     }
 }
