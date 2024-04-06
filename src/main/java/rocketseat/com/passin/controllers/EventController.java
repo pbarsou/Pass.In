653af63fb9d706e5f9ba57580ec4fb9a4430aad2
pbarsou <pablo.barsou@gmail.com>
2024-04-06 14:25:46 -0300

    Event Listing

--- src/main/java/rocketseat/com/passin/controllers/EventController.java
+++ src/main/java/rocketseat/com/passin/controllers/EventController.java
@@ -1,15 +1,21 @@
 package rocketseat.com.passin.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rocketseat.com.passin.services.EventService;
 
 @RestController
 @RequestMapping("/events") // mapeia os requests do endpoint mencionado, os quais o controller vai ser capaz de lidar
+@RequiredArgsConstructor
 public class EventController {
-    @GetMapping
-    public ResponseEntity<String> getTeste() {
+    private final EventService service;
+    @GetMapping("/{eventId}") // pega a partir da URL
+    public ResponseEntity<String> getEvent(@PathVariable String eventId) { // informando que a variável vai vir da URL (Path Variable)
+        this.service.getEventDetail(eventId);
         return ResponseEntity.ok("sucesso!");
     }
 }
--- src/main/java/rocketseat/com/passin/repositories/EventRepository.java
+++ src/main/java/rocketseat/com/passin/repositories/EventRepository.java
@@ -3,5 +3,7 @@ package rocketseat.com.passin.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rocketseat.com.passin.domain.event.Event;
 
+import java.util.UUID;
+
 public interface EventRepository extends JpaRepository<Event, String> {
 }
